a47198d4aa73d91ee572fc18a7ab29b06124c414
EricAdsuara <ericadsuara@gmail.com>
2026-07-31 03:37:00 +0800

    get length and set length function

--- src/main/java/org/example/Rectangle.java
+++ src/main/java/org/example/Rectangle.java
@@ -13,3 +13,13 @@ public class Rectangle {
         setWidth(width);
         this.filled = filled;
     }
+
+    public double getLength() { return length; }
+    public void setLength(double length) {
+        if (length <= 0) {
+            System.out.println("[Error] Length must be positive. Defaulting to 1.0.");
+            this.length = 1.0;
+            return;
+        }
+        this.length = length;
+    }
