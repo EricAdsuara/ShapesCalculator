62752dd13e3fb8026f66e3622b9682d73c9d2e9a
EricAdsuara <ericadsuara@gmail.com>
2026-07-31 02:44:59 +0800

    fixed == error

--- src/main/java/org/example/Square.java
+++ src/main/java/org/example/Square.java
@@ -33,7 +33,7 @@ public class Square {
         int size = 5;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                if (filled || i = 0 || i == size - 1 || j == 0 || j == size - 1) {
+                if (filled || i == 0 || i == size - 1 || j == 0 || j == size - 1) {
                     System.out.print("* ");
                 } else {
                     System.out.print("  ");
