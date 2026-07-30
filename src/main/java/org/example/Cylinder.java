bb60d1d7a35993cfa9d19d3503b87a4d73ae4166
EricAdsuara <ericadsuara@gmail.com>
2026-07-31 04:23:02 +0800

    get and set radius

--- src/main/java/org/example/Cylinder.java
+++ src/main/java/org/example/Cylinder.java
@@ -12,3 +12,11 @@ public class Cylinder {
         setRadius(radius);
         setHeight(height);
     }
+
+    public double getRadius() { return radius; }
+    public void setRadius(double radius) {
+        if (radius <= 0) {
+            System.out.println("[Error] Radius must be positive. Defaulting to 1.0.");
+            this.radius = 1.0;
+            return;
+        }
