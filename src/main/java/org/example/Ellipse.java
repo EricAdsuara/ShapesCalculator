67346648054e33b7825f940b729334be21296d38
EricAdsuara <ericadsuara@gmail.com>
2026-07-31 03:45:17 +0800

    set ellipse axis

--- src/main/java/org/example/Ellipse.java
+++ src/main/java/org/example/Ellipse.java
@@ -8,3 +8,9 @@ public class Ellipse {
     public Ellipse() {
         this(2.0, 1.0, true);
     }
+
+    public Ellipse(double semiMajorAxis, double semiMinorAxis, boolean filled) {
+        setSemiMajorAxis(semiMajorAxis);
+        setSemiMinorAxis(semiMinorAxis);
+        this.filled = filled;
+    }
