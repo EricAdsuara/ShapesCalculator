8b0b665820bcb9ca632434c57ecf44df1bd4b333
EricAdsuara <ericadsuara@gmail.com>
2026-07-31 03:08:47 +0800

    added system.out.print and do

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,16 @@
-package org.example;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        int choice;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+        do {
+            System.out.println("\n=== 2D SHAPES CALCULATOR ===");
+            System.out.println("1. Square\n2. Rectangle\n3. Circle\n4. Ellipse\n5. Triangle\n6. Exit");
+            Sytem.out.print("Select an option (1-6) ");
+
+            choice = scanner.ne
         }
     }
 }
