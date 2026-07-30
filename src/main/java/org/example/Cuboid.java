1ee3e476f84f86e889063137e8f8287792199b14
EricAdsuara <ericadsuara@gmail.com>
2026-07-31 04:13:37 +0800

    set lentgh width and height

--- src/main/java/org/example/Cuboid.java
+++ src/main/java/org/example/Cuboid.java
@@ -8,3 +8,9 @@ public class Cuboid {
     public Cuboid() {
         this(2.0, 1.0, 1.0);
     }
+
+    public Cuboid(double length, double width, double height) {
+        setLength(length);
+        setWidth(width);
+        setHeight(height);
+    }
