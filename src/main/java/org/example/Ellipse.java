63022aa6fac56179f3dc4be16267dea8e0203fa0
EricAdsuara <ericadsuara@gmail.com>
2026-07-31 03:45:02 +0800

    set ellipse size

--- src/main/java/org/example/Ellipse.java
+++ src/main/java/org/example/Ellipse.java
@@ -4,3 +4,7 @@ public class Ellipse {
     private double semiMajorAxis;
     private double semiMinorAxis;
     private boolean filled;
+
+    public Ellipse() {
+        this(2.0, 1.0, true);
+    }
