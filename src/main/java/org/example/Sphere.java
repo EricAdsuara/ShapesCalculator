ce1d19a2e006676a60a6a378ea8091b1cfa55396
EricAdsuara <ericadsuara@gmail.com>
2026-07-31 04:16:44 +0800

    get and set radius function

--- src/main/java/org/example/Sphere.java
+++ src/main/java/org/example/Sphere.java
@@ -10,3 +10,11 @@ public class Sphere {
     public Sphere(double radius) {
         setRadius(radius);
     }
+
+    public double getRadius() { return radius; }
+    public void setRadius(double radius) {
+        if (radius <= 0) {
+            System.out.println("[Error] Radius must be positive. Defaulting to 1.0.");
+            this.radius = 1.0;
+            return;
+        }
