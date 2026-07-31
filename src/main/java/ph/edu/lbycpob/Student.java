a048b085b78414bdbad268862e75f1f4fd2126a6
EricAdsuara <ericadsuara@gmail.com>
2026-07-31 11:55:15 +0800

    generates student class

--- src/main/java/ph/edu/lbycpob/Student.java
+++ src/main/java/ph/edu/lbycpob/Student.java
@@ -2,3 +2,8 @@ package ph.edu.lbycpob;
 
 import java.util.ArrayList;
 import java.util.List;
+
+public class Student {
+    private String studentId;
+    private String name;
+    private final List<CourseGrade> courses;
