73003c84e52503e516f74dd51cf973269bf55226
EricAdsuara <ericadsuara@gmail.com>
2026-07-31 11:58:14 +0800

    sets the available valid grades and returns if true

--- src/main/java/ph/edu/lbycpob/GradeValidator.java
+++ src/main/java/ph/edu/lbycpob/GradeValidator.java
@@ -2,3 +2,10 @@ package ph.edu.lbycpob;
 
 public class GradeValidator {
     private static final double[] VALID_GRADES = {0.0, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0};
+
+    public static boolean isValidGradePoint(double grade) {
+        for (double validGrade : VALID_GRADES) {
+            if (Double.compare(validGrade, grade) == 0) {
+                return true;
+            }
+        }
