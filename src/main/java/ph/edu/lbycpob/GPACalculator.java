57c7da5f3ea9133a4d1f23c27139d5f22ef6f63b
EricAdsuara <ericadsuara@gmail.com>
2026-07-31 12:00:07 +0800

    calculator class

--- src/main/java/ph/edu/lbycpob/GPACalculator.java
+++ src/main/java/ph/edu/lbycpob/GPACalculator.java
@@ -1,3 +1,10 @@
 package ph.edu.lbycpob;
 
 import java.util.List;
+
+public class GPACalculator {
+
+    public static double calculateGPA(List<CourseGrade> courses) {
+        if (courses == null || courses.isEmpty()) {
+            return 0.0;
+        }
